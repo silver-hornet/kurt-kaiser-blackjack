e1bd303517712db1142d14ba2d93f18870cb299f
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2020-10-07 09:12:27 -0400

    Initial commit

---
+++ Assets/Scripts/DeckScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckScript : MonoBehaviour
+{
+    public Sprite[] cardSprites;
+    int[] cardValues = new int[53];
+    int currentIndex = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void GetCardValues()
+    {
+        int num = 0;
+        // Loop to assign values to the cards
+        for (int i = 0; i < cardSprites.Length; i++)
+        {
+            num = i;
+            // Count up to the amount of cards, 52
+            num %= 13;
+            // If there is a remainder after x/13, then remainder
+            // is used as the value, unless over 10, then use 10
+            if(num > 10 || num == 0)
+            {
+                num = 10;
+            }
+            cardValues[i] = num++;
+        }
+        currentIndex = 1;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Button dealButton;
+    public Button hitButton;
+    public Button standButton;
+    public Button betButton;
+
+    void Start()
+    {
+        dealButton.onClick.AddListener(() => DealClicked());
+        hitButton.onClick.AddListener(() => HitClicked());
+        standButton.onClick.AddListener(() => StandClicked());
+    }
+
+    void DealClicked()
+    {
+        throw new NotImplementedException();
+    }
+
+    void HitClicked()
+    {
+        throw new NotImplementedException();
+    }
+
+    void StandClicked()
+    {
+        throw new NotImplementedException();
+    }
+}
